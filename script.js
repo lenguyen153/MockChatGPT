82f620a1ff3623bf1a53724c389a6bf9d3cb123b
Vincent Trinh <gawplayid154@gmail.com>
2024-03-19 15:51:06 -0400

    finish delete button

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-button");
 const themedButton = document.querySelector("#theme-button");
+const deleteButton = document.querySelector("#delete-button");
 const chatContainer = document.querySelector(".chat-container")
 
 const API_KEY = API_KEY1;
+const initialHeight = chatInput.scrollHeight;
 let userText = null;
 
 const loadDataFromLocalstorage = () => {
@@ -12,7 +14,13 @@ const loadDataFromLocalstorage = () => {
     document.body.classList.toggle("light-mode", themeColor === "light_mode");
     themedButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 
-    chatContainer.innerHTML = localStorage.getItem("all-chats")
+    const defaultText =
+        `<div class="default-text">
+            <h1>Mock ChatGPT </h1>
+            <p>Start a conversation and explore the power of AI.</p>
+        </div>`;
+
+    chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
 }
 
@@ -48,7 +56,8 @@ const getChatResponse = async (incomingChatDiv) => {
         // console.log(response);
         pElement.textContent = response.choices[0].message.content.trim();
     } catch (error) {
-        console.log(error);
+        pElement.classList.add("error");
+        pElement.textContent = "Oops! Something went wrong while retireving the response. Please try again later!"
     }
 
     // Remove typing animation, append the pararagraph element, save chat to local storage
@@ -93,8 +102,11 @@ const handleOutgoingChat = () => {
     // Create a new div and apply chat, specified class and set html content of div
     userText = chatInput.value.trim();
     // to avoid HTML format in the text
-    if(!userText) return 
-    // console.log(userText);
+    if (!userText) return
+
+    chatInput.value = "";
+    chatInput.style.height = `${initialHeight}px`;
+    
     const html =
         `<div class="chat-content">
             <div class="chat-details">
@@ -105,14 +117,37 @@ const handleOutgoingChat = () => {
     const outgoingChatDiv = createElement(html, "outgoing");
     // console.log(outgoingChatDiv);
     outgoingChatDiv.querySelector("p").textContent = userText;
+    document.querySelector(".default-text")?.remove();
     chatContainer.appendChild(outgoingChatDiv);
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
     setTimeout(showTypingAnimation, 500);
 }
 
-sendButton.addEventListener("click", handleOutgoingChat);
 themedButton.addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
     localStorage.setItem("theme-color", themedButton.innerText);
     themedButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+});
+
+// Remove all chat
+deleteButton.addEventListener("click", () => {
+    if (confirm("Are you sure you want to delete all the chats?")) {
+        localStorage.removeItem("all-chats");
+        loadDataFromLocalstorage();
+    }
+});
+
+
+chatInput.addEventListener("input", () => {
+    chatInput.style.height = `${initialHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
 })
+
+chatInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800){
+        e.preventDefault();
+        handleOutgoingChat();
+    }
+})
+
+sendButton.addEventListener("click", handleOutgoingChat);
